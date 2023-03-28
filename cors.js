f0c10bebf1d027f1a6ed3271ae633c618b751a2f
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-03-28 12:01:04 +0300

    Update cors.js

--- cors.js
+++ cors.js
@@ -13,3 +13,17 @@ fetch('https://site-with-cors.com')
   })
   .catch(error => console.error(error));
 // request to burp with base64
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Fetch and Base64 Example</title>
+</head>
+<body>
+  <script>
+    // Paste the JavaScript code here
+  </script>
+</body>
+</html>
+
+// node fetch-and-base64.js
